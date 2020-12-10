22fa89a7134084856e82a28630fb29ca5597ca2b
Chongyang Ma <machy85@gmail.com>
2020-12-09 21:03:28 -0800

    try to fix MSVC builds by adding _USE_MATH_DEFINES

--- src/DynamicElement/main.h
+++ src/DynamicElement/main.h
@@ -68,6 +68,7 @@ using std::ios;
 #endif
 #ifdef WIN32
 #include <windows.h>
+#define _USE_MATH_DEFINES
 #endif
 
 //------------------------------CONSTANTS---------------------------------------------
